23daf798b8ba0e3bddf5ec3ea76fd98a3a0fd5fd
JJZapatap <156350000+JJZapatap@users.noreply.github.com>
2026-07-22 16:08:38 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// SIRPC - Configuración
-// 1) Pegue aquí la URL de implementación de Google Apps Script que termina en /exec
-// 2) Ejemplo: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
-window.SIRPC_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw56L_4jmUttQrcx9r-t1IS2KPbFX9WNMsPrvWdohWG8UWwhWr1x2lP47KlUtbLI3Yd/exec"
-};
