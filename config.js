e85a319f43ba73ced2a72328586d54f3f2dd4315
JJZapatap <156350000+JJZapatap@users.noreply.github.com>
2026-07-22 14:20:55 -0500

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 1) Pegue aquí la URL de implementación de Google Apps Script que termina en /exec
 // 2) Ejemplo: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.SIRPC_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyLMImhHsODTpqCTLMM3JZj-GGMpnwyzKT2LLEgmiVRYJXNb_Z7wuQe9CbYCRoHTBFL/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzqUl478TJ4ZffjqdepEzeI3MRpvyzzD2LYWi6tDB0ZQWHs_1DRvtoqSaGvER_up87I/exec"
 };
